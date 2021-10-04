ad132c127ab77539d99d0e19b9adcbb3b29554d5
Nada Nour <64806089+nadaabdelreheem@users.noreply.github.com>
2021-10-04 19:22:56 +0200

    task 2-1

---
+++ next-char.php
@@ -0,0 +1,13 @@
+<?php
+    
+    $char = "m";
+
+    for($i="a";$i<="z";$i++){
+        
+        if ($char == $i){
+            
+             echo "Next character is: " . ++$i;
+            
+            }
+    }
+?>
